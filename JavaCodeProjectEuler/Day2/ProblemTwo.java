422059358ffc34a94be548aa075f88a4e1855956
BassamMutairi <mutairibassam@gmail.com>
2019-08-22 00:17:04 +0300

    Day-2

---
+++ JavaCodeProjectEuler/Day2/ProblemTwo.java
@@ -0,0 +1,36 @@
+/*	Day2 - Prblem 2
+	Problem statement:
+	By considering the terms in the Fibonacci sequence whose values do not exceed
+	four million, find the sum of the even-valued term.
+
+*/
+
+
+public class ProblemTwo {
+
+	
+	private static void problemTwoSolution() {
+		
+		// add two numbers
+		// add the total to the biggest number
+
+while(total < 10) {
+int num1 = 1;
+int num2 = 2;
+total = num1 + num2;
+total = total + num2;
+num2 = total;
+}
+		
+		System.out.println(total);
+	}
+
+	public static void main(String[] args) {
+	
+		problemTwoSolution();
+
+	}
+}
+
+
+
